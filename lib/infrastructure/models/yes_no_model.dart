0139c3be925f8cf2ee7881edb2b6a00a8deaf1df
doggbmx <manuelorticosta@gmail.com>
2023-08-17 18:33:09 -0400

    implement toMessageEntity method

--- lib/infrastructure/models/yes_no_model.dart
+++ lib/infrastructure/models/yes_no_model.dart
@@ -1,19 +1,31 @@
+import 'package:yes_no_app/domain/entities/message.dart';
+
 class YesNoModel {
-  String answer;
-  bool forced;
-  String imageUrl;
+  final String answer;
+  final bool forced;
+  final String image;
 
   YesNoModel({
     required this.answer,
     required this.forced,
-    required this.imageUrl,
+    required this.image,
   });
 
-  factory YesNoModel.fromJsonMap(Map<String, dynamic> json) {
-    return YesNoModel(
-      answer: json['answer'],
-      forced: json['forced'],
-      imageUrl: json['image'],
-    );
-  }
+  factory YesNoModel.fromJson(Map<String, dynamic> json) => YesNoModel(
+        answer: json["answer"],
+        forced: json["forced"],
+        image: json["image"],
+      );
+
+  Map<String, dynamic> toJson() => {
+        "answer": answer,
+        "forced": forced,
+        "image": image,
+      };
+
+  Message toMessageEntity() => Message(
+        text: answer == 'yes' ? 'Asi es' : 'Nope',
+        fromWho: FromWho.hers,
+        imageUrl: image,
+      );
 }
