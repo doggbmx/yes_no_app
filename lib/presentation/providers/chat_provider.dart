bdf9529519cc4965bd2a868c9bd496736b26e62a
doggbmx <manuelorticosta@gmail.com>
2023-08-17 18:12:02 -0400

    implement get_yes_no_answer

--- lib/presentation/providers/chat_provider.dart
+++ lib/presentation/providers/chat_provider.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:yes_no_app/config/helpers/get_yes_no_answer.dart';
 import 'package:yes_no_app/domain/entities/message.dart';
 
 class ChatProvider extends ChangeNotifier {
   final ScrollController chatScrollController = ScrollController();
+  final yesNoAnswer = GetYesNoAnswer();
 
   List<Message> messages = [
     Message(text: 'alo bb', fromWho: FromWho.mine),
@@ -13,8 +15,15 @@ class ChatProvider extends ChangeNotifier {
     if (text.isEmpty) return;
     final newMessage = Message(text: text, fromWho: FromWho.mine);
     messages.add(newMessage);
+    // we check if that was a question
+    if (text.endsWith('?')) await herReply();
+
     notifyListeners();
     moveScrollToBottom();
+  }
+
+  Future<void> herReply() async {
+    final herMessage = yesNoAnswer.getAnswer();
   }
 
   Future<void> moveScrollToBottom() async {
