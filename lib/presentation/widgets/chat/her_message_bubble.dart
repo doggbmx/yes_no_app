e34ef62dd396f91f67fcce221835e0f926d561f8
doggbmx <manuelorticosta@gmail.com>
2023-08-16 22:47:02 -0400

    implement img loadingProgress

--- lib/presentation/widgets/chat/her_message_bubble.dart
+++ lib/presentation/widgets/chat/her_message_bubble.dart
@@ -48,6 +48,19 @@ class _ImageBubble extends StatelessWidget {
         width: size.width * 0.7,
         height: 150,
         fit: BoxFit.cover,
+        loadingBuilder: (context, child, loadingProgress) {
+          if (loadingProgress == null) return child;
+
+          return Container(
+            width: size.width * 0.7,
+            height: 150,
+            padding: const EdgeInsets.symmetric(
+              horizontal: 10,
+              vertical: 5,
+            ),
+            child: const Text('Venom esta enviando una imagen'),
+          );
+        },
       ),
     );
   }
