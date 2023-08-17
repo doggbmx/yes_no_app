b6514e61a35189c36e448a5a967dcdb8e9ddaca4
doggbmx <manuelorticosta@gmail.com>
2023-08-17 17:22:51 -0400

    chat provider created

---
+++ lib/presentation/providers/chat_provider.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+import 'package:yes_no_app/domain/entities/message.dart';
+
+class ChatProvider extends ChangeNotifier {
+  List<Message> message = [
+    Message(text: 'alo bb', fromWho: FromWho.mine),
+    Message(text: 'ya regresaste del trabajo?', fromWho: FromWho.mine),
+  ];
+
+  Future<void> sendMessage(String text) async {
+    // TODO: IMPLEMENT THIS
+  }
+}
